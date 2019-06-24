8ed82bdde2ffae14867608144acecd3e7302409f
Akash <akashniki@gmail.com>
2019-06-24 21:32:13 +0000

    cepheus: User Taimen's build fingerprint for now

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -32,10 +32,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/cepheus/cepheus:9/PKQ1.181121.001/9.3.1:user/release-keys"
+BUILD_FINGERPRINT := google/taimen/taimen:9/PQ2A.190205.002/5164942:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cepheus-user 9 PKQ1.181121.001 9.3.1 release-keys" \
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ2A.190205.002 5164942 release-keys" \
     PRODUCT_NAME="cepheus" \
     TARGET_DEVICE="cepheus"
 
